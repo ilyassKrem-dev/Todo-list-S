e87898368ba70383a4be32a330d1a47a80185d9e
Ilyass <kreilyass@gmail.com>
2024-01-31 20:47:01 +0100

    Starting-project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,44 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1260px',
+      xxl: "1300px",  
+    },
     extend: {
+      colors: {
+        primary: '#131424',
+        secondary: '#393A47',
+        accent: '#F13024',
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        paint: 'url("/bg-paint.png")',
+      },
+      fontFamily: {
+        poppins: [`var(--font-poppins)`, 'sans-serif'],
+        sora: [`var(--font-sora)`, 'sans-serif'],
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  container: {
+    padding: {
+      DEFAULT: '15px',
+    },
+  },
+  plugins: [require('tailwind-scrollbar')],
+}
+export default config
